e0ae76192451f797f8c64face5ed217ea0fd59f8
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-07-24 10:07:42 -0300

    Next Step: HTML Tags 3

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A
-git commit -m "Next Step: Submitting a Form"
+git commit -m "Next Step: HTML Tags 3"
 git push -u origin main
 git status
 
-# continue in: https://academy.zerotomastery.io/courses/697434/lectures/12682795
+# continue in: https://academy.zerotomastery.io/courses/697434/lectures/12682799
